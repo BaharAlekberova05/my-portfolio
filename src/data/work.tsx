0fa5c62dd4e1e93909449dca8d39c37f72e8745a
BaharAlekberova <bahar.alekberova005@gmail.com>
2025-11-29 19:39:59 +0400

    correction

--- src/data/work.tsx
+++ src/data/work.tsx
@@ -7,7 +7,7 @@ export const workData = [
       "Created a modern and responsive website for Solution Appliances Repair, designed to attract customers and make service booking simple and intuitive. Emphasizes usability, clear navigation, and professional design.",
     link: "https://www.solutionappliancesrepair.com/",
     github: "/#",
-    language: "javascript",
+    language: "javascript" as "javascript",
   },
   {
     image: "/projects/dreamora.png",
@@ -17,7 +17,7 @@ export const workData = [
       "Created dreamora.az — a sleek, user-centered e-commerce platform engineered to attract customers, simplify online shopping, and deliver a seamless experience across all devices.",
     link: "https://dreamora.az/",
     github: "/#",
-    language: "typescript",
+    language: "typescript" as "typescript",
   },
   {
     image: "/projects/car-storm.png",
@@ -27,7 +27,7 @@ export const workData = [
       "Car Storm is a modern, responsive e-commerce website where users can browse, purchase, and wishlist cars. Admins can manage products from the dashboard, including adding, editing, and deleting cars. This application offers multiple features such as dark and light modes, multi-language support, product filters, authentication, and more.",
     link: "https://car-storm.vercel.app/",
     github: "https://github.com/BaharAlekberova05/Car-Storm-Website.git",
-    language: "javascript",
+    language: "javascript" as "javascript",
   },
   {
     image: "/projects/aub.png",
@@ -37,7 +37,7 @@ export const workData = [
       "Created aub.az — a caring and informative web portal for a children’s shelter, designed to raise awareness, support and facilitate communication with donors or volunteers. Emphasizes clarity, accessibility and social impact through thoughtful design.",
     link: "https://domain.aub.az/",
     github: "/#",
-    language: "typescript",
+    language: "typescript" as "typescript",
   },
   {
     image: "/projects/dentists.png",
@@ -46,8 +46,8 @@ export const workData = [
     description:
       "Created a modern, user-friendly dental appointment booking platform, designed to simplify scheduling, improve patient experience, and provide a seamless, responsive interface across devices.",
     link: "https://dentist-booking-site.vercel.app/",
-    github: "/https://github.com/BaharAlekberova05/Dentist-Booking-Site.git",
-    language: "javascript",
+    github: "https://github.com/BaharAlekberova05/Dentist-Booking-Site.git",
+    language: "javascript" as "javascript",
   },
   {
     image: "/projects/positivius.png",
@@ -56,8 +56,8 @@ export const workData = [
     description:
       "Built Positivus, a responsive website with HTML and Tailwind CSS, emphasizing clean design, mobile-first responsiveness, and smooth user experience.",
     link: "https://positivus-html-tailwind.vercel.app/",
-    github: "/https://github.com/BaharAlekberova05/positivus-html-tailwind.git",
-    language: "javascript",
+    github: "https://github.com/BaharAlekberova05/positivus-html-tailwind.git",
+    language: "javascript" as "javascript",
   },
   {
     image: "/projects/guess.png",
@@ -66,8 +66,8 @@ export const workData = [
     description:
       "A simple number guessing game built with HTML, CSS, and JavaScript.The goal is to guess a secret number between 1 and 20.This mini project is ideal for practicing DOM manipulation, working with event listeners, and using if-else conditions in JavaScript.",
     link: "https://guess-my-number-game-chi.vercel.app/",
-    github: "/https://github.com/BaharAlekberova05/Guess-my-number-game.git",
-    language: "javascript",
+    github: "https://github.com/BaharAlekberova05/Guess-my-number-game.git",
+    language: "javascript" as "javascript",
   },
   {
     image: "/projects/rpa.png",
@@ -75,10 +75,10 @@ export const workData = [
     name: "Rock Paper Scissors",
     description:
       "This project is a classic implementation of the Rock Paper Scissors game using HTML, CSS, and JavaScript. It's a fun and interactive game that allows users to play against the computer.",
-    link: "/https://rock-paper-scissors-game-js-ten.vercel.app/",
+    link: "https://rock-paper-scissors-game-js-ten.vercel.app/",
     github:
       "https://github.com/BaharAlekberova05/Rock-Paper-Scissors-Game-JS.git",
-    language: "javascript",
+    language: "javascript" as "javascript",
   },
   {
     image: "/projects/far-away.png",
@@ -87,7 +87,7 @@ export const workData = [
     description: "Simple to do list for travel.",
     link: "https://far-away-two-xi.vercel.app/",
     github: "https://github.com/BaharAlekberova05/far-away.git",
-    language: "javascript",
+    language: "javascript" as "javascript",
   },
   {
     image: "/projects/pig-game.png",
@@ -96,6 +96,6 @@ export const workData = [
     description: "An enjoyful game.",
     link: "https://pig-game-puce-eta.vercel.app/",
     github: "https://github.com/BaharAlekberova05/pig-game.git",
-    language: "javascript",
+    language: "javascript" as "javascript",
   },
 ];
